0f440769a5c6ec8894104b5685ca30c63733a0e2
Arpit <arpit.utkarsh49@gmail.com>
2025-06-26 20:30:54 +0530

    Created top doctors page again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import AboutHome from '../components/AboutHome'
 import Whychooseus from '../components/Whychooseus'
+import TopDoctors from '../components/TopDoctors'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <AboutHome />
       <SpecialityMenu />
       <Whychooseus />
+      <TopDoctors />
     </div>
   )
 }
