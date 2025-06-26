5e2071bc806e18f89a8845fc25d846347c4b4f02
Arpit <arpit.utkarsh49@gmail.com>
2025-06-26 20:36:46 +0530

    Created context for doctors

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import {doctors} from '../assets/assets/assets_frontend/assets'
+export const AppContext = createContext()
+
+const AppContextProvider = (props) => {
+    //whatever we  create in the value object we can access it in any component
+    const value = {
+        doctors
+    }
+
+    return(
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider
